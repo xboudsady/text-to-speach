340de22e7b66b2f3a738b3c87364c11e449947f8
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-10-15 17:15:07 -0700

    Added readme file

--- js/main.js
+++ js/main.js
@@ -9,9 +9,11 @@ const rate = document.querySelector('#rate');
 const rateValue = document.querySelector('#rate-value');
 const pitch = document.querySelector('#pitch');
 const pitchValue = document.querySelector('#pitch-value');
+const body = document.querySelector('body');
 
 // Initialize voices array
 
+// Global Array
 let voices = [];
 
 // Fetch the voices using the API
@@ -44,7 +46,74 @@ if (synth.onvoiceschanged !== undefined) {
 }
 
 
-// Speak
+// Speak function
 const speak = () => {
+    // Check if speaking, Using the built in method of the API
+    if (synth.speaking) {
+        console.error('Already speaking...');
+        return;
+    }
+    // Take whatever the value in the input field
+    if (textInput.value !== '') {
+        // Add background animation
+        body.style.background = '#141414 url(img/wave.gif)';
+        body.style.backgroundRepeat = 'repeat-x';
+        body.style.backgroundSize = '100% 100%';
 
-} 
+        // Get speak text using an new objet constructor
+        const speakText = new SpeechSynthesisUtterance(textInput.value);
+        
+        // Speak end
+        speakText.onend = e => {
+            console.log('Done Speaking...');
+            body.style.background = '#141414';
+        }
+
+        // Speak error
+        speakText.onerror = e => {
+            console.log('Something went wrong');
+        }
+
+        // Selected voice, Grab data from the getVoices() function during set Attribute run-time
+        const selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+        // Loop through the voice if the current iterations matches, choose that voice
+        voices.forEach(voice => {
+            if(voice.name === selectedVoice) {
+                // Assign voice to uor choosen selected name
+                speakText.voice = voice;
+            }
+        });
+
+        // Set pitch and rate
+        speakText.rate = rate.value;
+        speakText.pitch = pitch.value;
+
+        // Now speak, function calling itself (recursion)
+        synth.speak(speakText)
+    }
+} ;
+
+// EVENT LISTENERS - When Button is clicked, call the speak() function
+
+// Text form submit
+textForm.addEventListener('submit', e => {
+    e.preventDefault();
+    speak();
+    textInput.blur();
+});
+
+// Rate value change - Rate variable connected to slider
+rate.addEventListener('change', e => {
+    // Re-assign to the new value on slider
+    rateValue.textContent = rate.value
+});
+
+// Pitch value change - Pitch variable connected to slider
+pitch.addEventListener('change', e => {
+    // Re-assign to the new value on slider
+    pitchValue.textContent = pitch.value
+});
+
+// Voice select change
+voiceSelect.addEventListener('change', e => speak());
---
+++ readme.md
@@ -0,0 +1,37 @@
+# Text to Speech Apps
+
+Text Synthesis application, using the [Web Speech API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API)
+
+# Demo Site 
+[example]()
+
+## Technology Stack
+*  Front-End Component Libray - [Bootstrap](https://getbootstrap.com/)
+*  Bootstrap Dependancy - [Popper.js](https://popper.js.org/)
+*  Bootstrap Dependancy - [Popper.js](https://jquery.com/)
+*  Browser Technology - [Web Speech API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API)
+
+The **Web Speech API** enables you to incorporate voice data into web apps. The Web Speech API has two parts: 
+* **SpeechSynthesis** (Text-to-Speech)
+* **SpeechRecognition** (Asynchronous Speech Recognition.)
+
+`SpeechSynthesis`
+
+Interface of the **Web Speech API** is the controller interface for the speech service; this can be used to retrieve information about the synthesis voices available on the device, start and pause speech, and other commands besides.
+
+---
+
+### Methods
+`SpeechSynthesis.getVoices()`
+
+Returns a list of SpeechSynthesisVoice objects representing all the available voices on the current device.
+
+The `getVoices()` method of the SpeechSynthesis interface returns a list of SpeechSynthesisVoice objects representing all the available voices on the current device.
+
+
+`SpeechSynthesis.speak()`
+
+Adds an utterance to the utterance queue; it will be spoken when any other utterances queued before it have been spoken.
+
+## Screens
+
